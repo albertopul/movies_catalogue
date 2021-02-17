285653b14c34a0eb9ef0dcc9bd442e4ca6b307f2
Albert Pulinski <albertpulinski@gmail.com>
2021-02-17 22:27:15 +0100

    Add main.py and templates

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    movies = ["1", "2", "3", "4"]
+    return render_template("homepage.html", movies=movies)
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
